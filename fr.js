6143b82b822ec8bf01e5c60f06b1973e7c9ed823
Piotr Leś <58136931+Piotr-Les@users.noreply.github.com>
2020-04-29 16:43:30 +0200

    added files

---
+++ fr.js
@@ -0,0 +1,57 @@
+const name = document.querySelector("#name");
+const mess = document.querySelector("#mess");
+const formcont = document.querySelector
+  ("#form-cont");
+const sub = document.querySelector("#sub");
+const cake = document.querySelector("#cake-cont");
+
+document.addEventListener('submit', sendMess);
+sub.addEventListener('click', getUser)
+async function sendMess(e)
+{
+  e.preventDefault();
+  let namev = name.value;
+  let messv = mess.value;
+  fetch('users.json',
+    {
+      method: 'POST',
+      headers: {
+        'Accept': 'aplication/json, text/plain, */*',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ name: name, mess: mess })
+    })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+
+  formcont.style.display = "none";
+
+  name.value = "";
+  mess.value = "";
+
+}
+async function getUser()
+{
+  cake.classList.toggle("dis-none")
+  fetch("users.json")
+    .then(function (res)
+    {
+      return res.json()
+    })
+    .then((data) =>
+    {
+      let output = "<h2>Users</h2>";
+      data.forEach(function (user)
+      {
+        output += `
+        <ul style="list-style:none">
+        <li>ID: ${user.id}</li>
+        <li>Name: ${user.name}</li>
+        <li>Mess: ${user.mess}</li>
+        </ul>
+        `
+      });
+      document.querySelector("#cake-cont").innerHTML = output;
+    })
+}
---
+++ ui.js
@@ -0,0 +1,70 @@
+
+// burger
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+burger.addEventListener('click', function ()
+{
+  // navmenu animation
+  nav.classList.toggle('nav-active');
+  navLinks.forEach((link, index) =>
+  {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+    }
+  });
+  // burger animation
+  burger.classList.toggle('toggle');
+});
+
+
+// logo i navbar
+const Logo = document.querySelector("#logo-img");
+const navbar = document.querySelector("nav");
+let endOfDocumentTop = 80;
+let size = 0;
+let clwidth = document.documentElement.clientWidth;
+//
+window.onload = function ()
+{
+  const Logo = document.querySelector("#logo-img");
+  let scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  if (clwidth >= 1025) {
+    if (scroll == 0) {
+      Logo.className = "logo-big";
+      Logo.style.position = "absolute"
+    }
+  }
+}
+
+window.onscroll = function ()
+{
+  if (clwidth >= 1025) {
+    growShrinkLogo()
+  }
+  else {
+    const Logo = document.querySelector("#logo-img");
+    const navbar = document.querySelector("nav");
+    Logo.className = "logo-tiny";
+    Logo.style.top = "auto"
+    navbar.style.height = "10vh"
+  }
+};
+
+function growShrinkLogo()
+{
+  let scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  if (size == 0 && scroll > endOfDocumentTop) {
+    Logo.className = "logo-tiny";
+    navbar.style.height = "9vh"
+    size = 1;
+  } else if (size == 1 && scroll <= endOfDocumentTop) {
+    Logo.className = "logo-big";
+    Logo.style.position = "absolute"
+    Logo.style.top = "0"
+    navbar.style.height = "10vh"
+    size = 0;
+  }
+}
